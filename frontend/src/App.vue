b02ad00dab0eafe8408b5ab4cd3288adf77a46b8
JoaoLeonello <jfelipe.pl@gmail.com>
2020-10-21 20:16:22 +0000

    vue setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Encoder class="encode"></Encoder>
+  </div>
+</template>
+
+<script>
+import Encoder from './components/Encoder.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Encoder
+  }
+}
+</script>
+
+<style>
+  #app {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 80px;
+  }
+
+  .encode {
+    align-self: center;
+  }
+</style>
---
+++ frontend/src/components/Encoder.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="wrapper">
+      <!-- <form class="form" @submit.prevent="save"> -->
+        <h3 class='subtitle'>URL Encoder</h3>
+          <div class="control">
+            <input class="input" type="text" v-model="url">
+            <button class="button-is-success" v-on:click="shortUrl">Encode</button>
+          </div> 
+      <!-- </form> -->
+    </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({ 
+      url: ""
+    }),
+    methods: {
+      shortUrl: function (event) {
+        return axios.post('http://localhost:3000/short-url', this.url)
+      }
+    }
+  }
+</script>
+  
+<style scoped>
+  .wrapper {
+    background-color: whitesmoke;
+    padding: 13px;
+    border-radius: 15px;
+    width: 300px;
+  }
+
+  .control {
+    display: flex;
+    
+  }
+
+  .input {
+    align-self: flex-start;
+  }
+
+  .button-is-success {
+    align-self: flex-end;
+    height: 34px;
+    margin-left: 10px;
+    background-color: white;
+    border-radius: 6px;
+    font-family: 'Roboto', sans-serif;;
+    font-weight: 500;
+    
+  }
+
+  .subtitle {
+    text-align: center;
+    font-family: 'Roboto', sans-serif;
+  }
+
+</style>
